012fa9376d798a83ae297c4706a8f2509a63fb35
hyunjin <alfma123@naver.com>
2022-09-30 13:33:01 +0900

    refactored by map

--- index_refactor.js
+++ index_refactor.js
@@ -1,38 +1,43 @@
 var users = [
-  { id: 1, name: "ID", age: 32 },
-  { id: 2, name: "HA", age: 25 },
-  { id: 3, name: "BJ", age: 32 },
-  { id: 4, name: "PJ", age: 28 },
-  { id: 5, name: "JE", age: 27 },
-  { id: 6, name: "JM", age: 32 },
-  { id: 7, name: "HI", age: 24 },
+    { id: 1, name: 'ID', age: 32 },
+    { id: 2, name: 'HA', age: 25 },
+    { id: 3, name: 'BJ', age: 32 },
+    { id: 4, name: 'PJ', age: 28 },
+    { id: 5, name: 'JE', age: 27 },
+    { id: 6, name: 'JM', age: 32 },
+    { id: 7, name: 'HI', age: 24 },
 ];
 function filter(list, predicate) {
-  var new_list = [];
-  for (var i = 0, len = list.length; i < len; i++) {
-    if (predicate(list[i])) new_list.push(list[i]);
-  }
-  return new_list;
+    var new_list = [];
+    for (var i = 0, len = list.length; i < len; i++) {
+        if (predicate(list[i])) new_list.push(list[i]);
+    }
+    return new_list;
+}
+function map(list, iteratee) {
+    var new_list = [];
+    for (var i = 0, len = list.length; i < len; i++) {
+        new_list.push(iteratee(list[i]));
+    }
+    return new_list;
 }
 
 var users_under_30 = filter(users, function (user) {
-  return user.age < 30;
+    return user.age < 30;
 });
 console.log(users_under_30.length);
 
-var ages = [];
-for (var i = 0, len = users_under_30.length; i < len; i++) {
-  ages.push(users_under_30[i].age);
-}
+var ages = map(users_under_30, function (user) {
+    return user.age;
+});
 console.log(ages);
 
 var users_over_30 = filter(users, function (user) {
-  return user.age >= 30;
+    return user.age >= 30;
 });
 console.log(users_over_30.length);
 
-var names = [];
-for (var i = 0, len = users_over_30.length; i < len; i++) {
-  names.push(users_over_30[i].name);
-}
+var names = map(users_over_30, function (user) {
+    return user.name;
+});
 console.log(names);
--- note.js
+++ note.js
@@ -3,3 +3,11 @@ function addMaker(a) {
         return a + b;
     };
 }
+
+addMaker(10)(5);
+
+var add5 = addMaker(5);
+add5(3);
+add5(4);
+
+console.log(add5(3), add5(4));
