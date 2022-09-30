359205ea4fa561795a96870a5903eedd6afdb413
hyunjin <alfma123@naver.com>
2022-09-30 14:39:37 +0900

    bvalue

---
+++ bvalue.js
@@ -0,0 +1,12 @@
+function addMaker(a) {
+    return function (b) {
+        return a + b;
+    };
+}
+function bvalue(key) {
+    return function (obj) {
+        return obj[key];
+    };
+}
+bvalue('a')({ a: 'hi', b: 'hello' });
+console.log(bvalue('a')({ a: 'hi', b: 'hello' }));
