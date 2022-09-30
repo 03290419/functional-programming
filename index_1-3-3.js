c7045aba5a5266d3669102f750fa2cbde7dd9e1d
hyunjin <alfma123@naver.com>
2022-09-30 18:12:12 +0900

    bmatch picks various data

--- index_1-3-3.js
+++ index_1-3-3.js
@@ -21,16 +21,35 @@ function map(list, iteratee) {
     return new_list;
 }
 
-function bmatch1(key, val) {
-    return function (obj) {
-        return obj[key] === val;
-    };
-}
 function object(key, val) {
     var obj = {};
     obj[key] = val;
     return obj;
 }
-
-console.log(filter(users, bmatch1('age', 32)));
-console.log(map(users, bmatch1('age', 32)));
+function match(obj, obj2) {
+    for (var key in obj2) {
+        if (obj[key] !== obj2[key]) return false;
+    }
+    return true;
+}
+function bmatch(obj2, val) {
+    if (arguments.length == 2) obj2 = object(obj2, val);
+    return function (obj) {
+        return match(obj, obj2);
+    };
+}
+console.log(
+    match(
+        find(
+            users,
+            bmatch('id', 3),
+            find(users, bmatch(users, bmatch('name', 'BJ')))
+        )
+    )
+);
+console.log(
+    find(users, function (u) {
+        return u.age === 32 && u.name === 'JM';
+    })
+);
+console.log(find(users, bmatch({ name: 'JM', age: 32 })));
