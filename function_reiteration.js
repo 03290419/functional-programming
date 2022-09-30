540c211c2a6809af53b66f9581e5cb86fdd0ebba
hyunjin <alfma123@naver.com>
2022-09-30 13:53:08 +0900

    delete variables

--- function_reiteration.js
+++ function_reiteration.js
@@ -1,21 +1,29 @@
-var ages = map(
-    filter(users, function (user) {
-        return user.age < 30;
-    }),
-    function (user) {
-        return user.age;
-    }
+function log_length(value) {
+    console.log(value.length);
+    return value;
+}
+console.log(
+    log_length(
+        map(
+            filter(users, function (user) {
+                return user.age < 30;
+            }),
+            function (user) {
+                return user.age;
+            }
+        )
+    )
 );
-console.log(ages.length);
-console.log(ages);
 
-var names = map(
-    filter(users, function (user) {
-        return user.age >= 30;
-    }),
-    function (user) {
-        return user.name;
-    }
+console.log(
+    log_length(
+        map(
+            filter(users, function (user) {
+                return user.age >= 30;
+            }),
+            function (user) {
+                return user.name;
+            }
+        )
+    )
 );
-console.log(names.length);
-console.log(names);
