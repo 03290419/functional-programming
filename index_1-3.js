82534f10bc46279b280167cba9312bd185e96078
hyunjin <alfma123@naver.com>
2022-09-30 17:25:34 +0900

    limits of findBy

--- index_1-3.js
+++ index_1-3.js
@@ -1,32 +1,30 @@
 import { users } from './users.js';
 
-function findById(list, id) {
-    for (var i = 0, len = list.length; i < len; i++) {
-        if (list[i].id === id) return list[i];
-    }
-}
-console.log(findById(users, 3));
-console.log(findById(users, 5));
-function findByName(list, name) {
-    for (var i = 0, len = list.length; i < len; i++) {
-        if (list[i].name === name) return list[i];
-    }
-}
-console.log(findByName(users, 'BJ'));
-console.log(findByName(users, 'JE'));
-function findByAge(list, age) {
-    for (var i = 0, len = list.length; i < len; i++) {
-        if (list[i].age === age) return list[i];
-    }
-}
-console.log(findByAge(users, 28));
-console.log(findByAge(users, 25));
-
 function findBy(key, list, val) {
     for (var i = 0, len = list.length; i < len; i++) {
         if (list[i][key] === val) return list[i];
     }
 }
-console.log(findBy('name', users, 'BJ'));
-console.log(findBy('id', users, 2));
-console.log(findBy('age', users, 28));
+
+function User(id, name, age) {
+    this.getId = function () {
+        return id;
+    };
+    this.getName = function () {
+        return name;
+    };
+    this.getAge = function () {
+        return age;
+    };
+}
+
+var users2 = [
+    new User(1, 'ID', 32),
+    new User(2, 'HA', 25),
+    new User(3, 'BJ', 32),
+    new User(4, 'PJ', 28),
+    new User(5, 'JE', 27),
+    new User(6, 'JM', 32),
+    new User(7, 'HI', 24),
+];
+console.log(findBy('age', users2, 25));
