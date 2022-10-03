2cfd9ce12ad950c382c099d2307a6a49d1d27c73
hyunjin <alfma123@naver.com>
2022-10-03 19:55:57 +0900

    :sparkles: some every beg not
    some every beg not

--- identity.js
+++ identity.js
@@ -1,4 +1,5 @@
-import { filter } from './likeUnderscore.js';
+import _ from 'underscore';
+import { filter, find } from './likeUnderscore.js';
 
 const identity = function (v) {
     return v;
@@ -14,3 +15,26 @@ const falsy = function (v) {
 const truthy = function (v) {
     return !!v;
 };
+
+const some = function (list) {
+    return !!find(list, identity);
+};
+const every = function (list) {
+    return filter(list, identity).length == list.length;
+};
+
+console.clear();
+console.log(some([0, null, 2]));
+console.log(some([0, null, false]));
+
+console.log(every([0, null, 2]));
+console.log(every([{}, true, 2]));
+
+function not(v) {
+    return !v;
+}
+function beq(a) {
+    return function (b) {
+        return a === b;
+    };
+}
