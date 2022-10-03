f2ac5e47876c2106874855436516915b48e9589c
hyunjin <alfma123@naver.com>
2022-10-03 20:26:53 +0900

    :pencil2: tested not logic in note.js
    tested not logic in note.js

--- note.js
+++ note.js
@@ -1,13 +1,37 @@
-function addMaker(a) {
+export const findIndex = function (list, predicate) {
+    for (var i = 0, len = list.length; i < len; i++) {
+        // if (predicate(list[i], i, list)) return i;
+        console.log(
+            predicate(list[i], i, list),
+            'list[i]-' + list[i],
+            'i-' + i,
+            'list-' + list
+        );
+    }
+    // return -1;
+};
+
+function positive(list) {
+    return find(list, identity);
+}
+function negativeIndex(list) {
+    return findIndex(list, not);
+}
+const some = function (list) {
+    return not(not(positive(list)));
+};
+const every = function (list) {
+    return beg(-1)(negativeIndex(list));
+};
+
+every([{}, true, 2]);
+
+function not(v) {
+    console.log('not 이 받은 값' + v);
+    return !v;
+}
+function beg(a) {
     return function (b) {
-        return a + b;
+        return a === b;
     };
 }
-
-addMaker(10)(5);
-
-var add5 = addMaker(5);
-add5(3);
-add5(4);
-
-console.log(add5(3), add5(4));
